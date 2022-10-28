a59ce91471b044d518b7e66b1d3b5c12444130dd
BurhanuddinRaja <mails4burhan@gmail.com>
2022-10-28 18:30:05 +0530

    Check for Sudoku added

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+// Test Arrays
 let sudoku: Array<Array<number>> = [[0, 0, 4, 5, 6, 0, 0, 0, 9],
 [0, 5, 6, 0, 0, 0, 2, 0, 3],
 [7, 8, 9, 3, 0, 4, 1, 0, 0],
@@ -27,7 +28,7 @@ let sudokuAnswer = document.querySelector('.answer-question')
 let startBtn = document.querySelector('.start-btn-active button')
 let startBtnCont = document.querySelector('.start-btn-active')
 
-// Check or Get Answer Btn
+// To get Check and Get Answer Btn
 let answerCont = document.querySelector('.check-answer')
 
 // To Start the Game
@@ -51,19 +52,18 @@ const createBoard = (n: number): void => {
     answerCont?.classList.add('check-answer-active')
 
     let questionBoard = document.querySelector('.question-container-active .container')
-    
+
     let divTag = `<div></div>`
     for (let i = 1; i <= n; i++) {
         questionBoard?.insertAdjacentHTML('beforeend', divTag)
     }
-    // console.log(questionBoard)
     let numBoard = document.querySelectorAll('.question-container-active .container div')
-    
+
     let count: number = 0
     for (let i = 0; i < sudoku.length && count <= n; i++) {
         for (let j = 0; j < sudoku.length && count <= n; j++) {
             if (sudoku[i][j] === 0) {
-                numBoard[count].insertAdjacentHTML("beforeend", `<input type="text" />`)
+                numBoard[count].insertAdjacentHTML("beforeend", `<input id="${count}" type="text" />`)
             }
             else {
                 numBoard[count].insertAdjacentHTML("beforeend", `${sudoku[i][j]}`)
@@ -71,9 +71,63 @@ const createBoard = (n: number): void => {
             count++
         }
     }
+    getData()
 }
 
+// To get the data from input
+let inputArray: Array<number> = new Array(81).fill(0)
 
+const getData = () => {
+    let inputNum = document.querySelectorAll('.question-container-active .container div input')
+    inputNum.forEach(inp => {
+        inp.addEventListener("change", () => {
+
+            let input = <HTMLInputElement>inp
+            inputArray.splice(Number(input.id), 1, Number(input.value))
+        })
+    })
+}
+
+// To Check the given answer
+let checkAnsBtn = document.querySelector('.check')
+let wrongAns = document.querySelector('.wrong')
+let correctAns = document.querySelector('.correct')
+
+checkAnsBtn?.addEventListener('click', () => {
+    let answer: boolean = checkAns()
+    if (answer) {
+        correctAns?.classList.remove('correct')
+        correctAns?.classList.add('correct-active')
+    }
+    else {
+        wrongAns?.classList.remove('wrong')
+        wrongAns?.classList.add('wrong-active')
+    }
+})
+
+// Check answer function
+const checkAns = (): boolean => {
+    solve()
+    let count: number = 0
+    for (let i = 0; i < sudoku.length; i++) {
+        let flag: boolean = false
+        for (let j = 0; j < sudoku.length; j++) {
+            if (inputArray[count] !== 0 && inputArray[count] == sudokuAnswerArray[i][j]) {
+                flag = true
+                console.log(true)
+            }
+            if (inputArray[count] !== 0 && inputArray[count] !== sudokuAnswerArray[i][j]) {
+                flag = false
+                break
+            }
+            count++
+        }
+        if (!flag) {
+            return false
+        }
+    }
+    return true
+}
 
 
 // Solve Sudoku
@@ -122,13 +176,13 @@ const isSafe = (i: number, j: number, n: number): boolean => {
     }
 
     let s: number = Math.sqrt(sudokuAnswerArray.length)
-    let rs: number = i - (i%s)
-    let cs: number = j - (j%s)
+    let rs: number = i - (i % s)
+    let cs: number = j - (j % s)
 
     // Checking in a particular box for similar number as 'n' 
     for (let k = 0; k < s; k++) {
         for (let l = 0; l < s; l++) {
-            if (sudokuAnswerArray[k+rs][l+cs] === n) {
+            if (sudokuAnswerArray[k + rs][l + cs] === n) {
                 return false;
             }
         }
